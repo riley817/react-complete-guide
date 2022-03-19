a41dc09c8afab354589c42be80a97aa277d641a1
riley <riley0422@icloud.com>
2022-03-19 22:57:00 +0900

    20220319225657

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ExpenseItem from './components/ExpenseItem';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -9,7 +10,9 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+
+
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
