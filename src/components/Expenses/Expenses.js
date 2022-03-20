d0be0d1bf82598a33391e7aa9e9e9d4ead41b937
riley <riley0422@icloud.com>
2022-03-20 21:56:23 +0900

    Update modules

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 import './Expenses.css';
 
-function Expense(props) {
+const Expense = (props) => {
     return (
         <Card className='expenses'>
             <ExpenseItem
