e8c5514af12a15c4141dcba3ad36a68e58b16537
riley <riley0422@icloud.com>
2022-03-22 23:10:41 +0900

    20220322231039

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,17 @@
+import React, { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        console.log('Clicked!')
+    };
+    
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
@@ -10,7 +19,8 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
-export default ExpenseItem;
+export default ExpenseItem; 
