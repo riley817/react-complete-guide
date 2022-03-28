9d72010b9e7be721d2df93f66992d828d967cb53
riley <riley0422@icloud.com>
2022-03-28 23:16:00 +0900

    20220328231558

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,17 +6,20 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+    // 개별상태값을 관리하므로 const로 관리 
     const [title, setTitle] = useState(props.title);
+    console.log('ExpenseItem evaluated by React');
 
     const clickHandler = () => {
-        console.log('Clicked!')
+        setTitle('Updated!');
+        console.log(title);
     };
     
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
